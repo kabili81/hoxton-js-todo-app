05407f161685a672f3ca937ce9a76f29e9680525
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-26 23:22:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
+let state = {
+    todos: [
+        {
+            text: 'Go Shopping',
+            completed: false,
+        },
+        {
+            text: 'Work out',
+            completed: false,
+        },
+        {
+            text: 'See the Doctor',
+            completed: true,
+        },
+    ],
+    showCompleted:false,
+}
+
+function toggleShowCompleted() {
+    state.showCompleted = !state.showCompleted
+    render()
+}
+
+function addTodo(text) {
+    if (text.length === 0) return
+    let todo = {
+        text: text,
+        completed: false,
+    }
+    state.todos.push(todo)
+    render()
+}
+
+function renderApp() {
+    let appEl = document.createElement('main')
+    appEl.className = 'app'
+    document.body.append(appEl)
+}
 
 
 function renderOptionsSection() {
-   // <section>
-    //      <h2 class="title">OPTIONS</h2>
-    //      <label>
-    //        Show completed
-    //        <input type="checkbox" checked />
-    //      </label>
-    //    </section>
+  
     let optionsSection = document.createElement('section')
     optionsSection.classList.add('options')
 
@@ -28,11 +60,6 @@ function renderOptionsSection() {
 
 }
 
-function renderApp() {
-    let appEl = document.createElement('main')
-    appEl.className = 'app'
-    document.body.append(appEl)
-}
 
 function renderAddTodoSection() {
     //    <section>
@@ -52,32 +79,47 @@ function renderAddTodoSection() {
 
 function renderTodoSection() {
     
-//     <section>
-//       <h2 class="title">TODO</h2>
-//       <ul class="todo-list">
-//       <li class="todo">
-//         <input type="checkbox" />
-//         <p>Go shopping</p>
-//         <button>Delete</button>
-//       </li>
-//       <li class="todo">
-//         <input type="checkbox" />
-//        <p>Work out</p>
-//        <button>Delete</button>
-//      </li>
-//    </ul>
-//    </section>
+
+
+let todoSection = document.createElement('section')
+
+let titleEL = document.createElement = ('h2')
+titleEL.classname = 'section__title'
+titleEL.innerText = 'Todo'
+
+let todoListEl = document.createElement('ul')
+todoListEl.className = 'todo-list'
+
+let incompleTodos = state.todos.filter (todo => todo.completed === false)
+
+for (let todo of incompleTodos) {
+    let todoItemEl = document.createElement('li')
+    todoItemEl.className = 'todo'
+
+    let checkboxEl = document.createElement('input')
+    checkboxEl.type = 'checkbox'
+    checkboxEl.className = 'todo__checkbox'
+
+    let todoTextEl = document.createElement('p')
+    todoTextEl.className = 'todo__delete'
+    todoTextEl.innerText = 'Delete'
+
+    let deleteButtonEl = document.createElement('button')
+    deleteButtonEl.className = 'todo__delete'
+    deleteButtonEl.innerText = 'Delete'
+
+    todoItemEl.append(checkboxEl, todoTextEl, deleteButtonEl)
+
+    todoListEl.append(todoItemEl)
 }
 
-function ren
+todoSection.append(titleEL, todoListEl)
 
-function render() {
-    document.body.textContent = ''
+let appEl = document.querySelector('.app')
+appEl.append(todoSection)
+}
 
-    renderApp()
-    renderOptionsSection()
-   
-
+function renderCompletedTodosSecition() {
 //    <section>
 //      <h2 class="title">COMPLETED</h2>
 //      <ul class="completed-todo-list">
@@ -88,6 +130,17 @@ function render() {
 //        </li>
 //      </ul>
 //    </section>
+
+}
+
+function render() {
+    document.body.textContent = ''
+
+renderApp()
+renderOptionsSection()
+renderTodoSection()      
+
+
 //  </main>
 }
 
